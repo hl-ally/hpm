631473f2c772b1a0ed2f3a61f6f60fea6889c755
hul <hul@zyitc.cn>
2023-09-20 09:53:19 +0800

    分系列管控工程文件夹

--- project/getSiFileList.cmd
+++
@@ -1,3 +0,0 @@
-python HPMProject_si4project_filelist.py D:\code\github\HPM\project\hpm6750evk_build\common_procect.json
-
-pause
---
+++ project_hpm6750evk/getSiFileList.cmd
@@ -0,0 +1,3 @@
+python HPMProject_si4project_filelist.py D:\code\github\HPM\project_hpm6750evk\hpm6750evk_build\hpm6750evk.json
+
+pause
